d845844144ef837397d88b8aa397ec0f58c03d85
imAsparky <mark.sevelj@dunwright.com.au>
2024-04-19 04:31:48 +0800

    chore(image): Add comprehensive image upload checks, exceptions and logging #31
    closes #31

---
+++ django_ckeditors/exceptions.py
@@ -0,0 +1,16 @@
+class PillowImageError(Exception):
+    """Catchall for when a Pillow read error is encountered.
+
+    """
+
+    def __init__(self, message, image_type=None):
+        super().__init__(message)
+        self.image_type = image_type
+
+
+class InvalidImageTypeError(Exception):
+    """Raised when an unsupported image type is encountered."""
+
+    def __init__(self, message, image_type=None):
+        super().__init__(message)
+        self.image_type = image_type
--- django_ckeditors/urls.py
+++ django_ckeditors/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("image_upload/", views.upload_file, name="ck_editors_upload_image"),
+    path("image_upload/", views.upload_image, name="ck_editors_upload_image"),
 ]
