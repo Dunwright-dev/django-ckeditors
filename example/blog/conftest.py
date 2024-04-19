dfff80fce95ec7ab8660ceced7f7b75788eb8f70
imAsparky <mark.sevelj@dunwright.com.au>
2024-04-19 17:37:47 +0800

    tests(helper): Add tests and fixtures #38
    I have also added a corrupt image file generator to expand tests for corrupted images at a later date.
    
    closes #38

--- example/blog/conftest.py
+++ example/blog/conftest.py
@@ -7,8 +7,38 @@ from django_ckeditors.forms import UploadFileForm
 
 
 @pytest.fixture()
-def image_png():
-    file_path = os.path.join(os.path.dirname(__file__), "fixtures", "files", "test.png")
+def valid_png_image():
+    file_path = os.path.join(
+        os.path.dirname(__file__),
+        "fixtures",
+        "files",
+        "images",
+        "test.png",
+    )
+    return open(file_path, "rb")
+
+
+@pytest.fixture()
+def invalid_jpg_image():
+    file_path = os.path.join(
+        os.path.dirname(__file__),
+        "fixtures",
+        "files",
+        "images",
+        "this_is_a_pdf_file.jpg",
+    )
+    return open(file_path, "rb")
+
+
+@pytest.fixture()
+def corrupted_png_image():
+    file_path = os.path.join(
+        os.path.dirname(__file__),
+        "fixtures",
+        "files",
+        "images",
+        "test_corrupted.png",
+    )
     return open(file_path, "rb")
 
 
