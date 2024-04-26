94cb56404d129033088ed53e4f523db3562b217f
imAsparky <mark.sevelj@dunwright.com.au>
2024-04-27 04:11:38 +0800

    tests(helpers): Add tests for helpers #40 #42
    Add/update tests for the following.
    
    Refactor get storage class to return an object so tests can be conducted using @override_settings.
    
    Add option to use a custom url for saving CKE image file.
    
    closes #40
    
    closes #42

--- example/blog/tests/test_helpers.py
+++ example/blog/tests/test_helpers.py
@@ -1,55 +1,117 @@
-from unittest.mock import MagicMock, patch
+import os
+import shutil
 
-import pytest
-from django.contrib.auth.models import AnonymousUser
-from django.test import RequestFactory
+from django.conf import settings
 
-from django_ckeditors.exceptions import InvalidImageTypeError, PillowImageError
-from django_ckeditors.helpers import has_permission_to_upload_images, image_verify
+# from django.core.files.storage import Storage
+from django.core.files.uploadedfile import SimpleUploadedFile
+from django.test import RequestFactory, SimpleTestCase, override_settings
+
+from django_ckeditors.helpers import (
+    handle_uploaded_image,
+)
+
+# class TestHasPermissionToUploadImages:
+#     @staticmethod
+#     def test_non_staff_user_without_setting():
+#         request = RequestFactory().get("/")
+#         request.user = AnonymousUser()  # Simulate a non-authenticated user
+#
+#         assert (
+#             has_permission_to_upload_images(request) is True
+#         )  # Should have permission
+#
+#     @staticmethod
+#     @patch("django.conf.settings.DJ_CKE_STAFF_ONLY_IMAGE_UPLOADS", new=True)
+#     def test_non_staff_user_with_setting():
+#         request = RequestFactory().get("/")
+#         request.user = AnonymousUser()
+#
+#         assert has_permission_to_upload_images(request) is False
+#
+#     @staticmethod
+#     @patch("django.conf.settings.DJ_CKE_STAFF_ONLY_IMAGE_UPLOADS", new=True)
+#     def test_staff_user():
+#         request = RequestFactory().get("/")
+#         request.user = MagicMock(is_staff=True)  # Simulate staff user
+#
+#         assert has_permission_to_upload_images(request) is True
+#
+#
+# class TestImageVerify:
+#     @staticmethod
+#     def test_valid_filetype(valid_png_image):
+#
+#         image_verify(image=valid_png_image)  # Should execute without raising exceptions  # noqa: E501
+#
+#     @staticmethod
+#     def test_invalid_filetypes(invalid_jpg_image):
+#         """invalid_jpg_image is a pdf file with jpg extension."""
+#
+#         with pytest.raises(InvalidImageTypeError):
+#             image_verify(invalid_jpg_image)
+#
+#     @staticmethod
+#     def test_corrupt_png(corrupted_png_image):
+#         """corrupted_png_image is a png with random bytes changed."""
+#         with pytest.raises(PillowImageError):
+#             image_verify(corrupted_png_image)
+#
 
 
-class TestHasPermissionToUploadImages:
-    @staticmethod
-    def test_non_staff_user_without_setting():
-        request = RequestFactory().get("/")
-        request.user = AnonymousUser()  # Simulate a non-authenticated user
+class HandleUploadedImageTests(SimpleTestCase):
+    @override_settings(
+        DJ_CKE_IMAGE_STORAGE="",
+        STORAGES={
+            "default": {"BACKEND": "django.core.files.storage.FileSystemStorage"},
+        },
+    )
+    def test_default_upload_url(self):
+        # Create test image data
+        image_data = b"Dummy image data"
+        image_file = SimpleUploadedFile("test_image.jpg", image_data)
 
-        assert (
-            has_permission_to_upload_images(request) is True
-        )  # Should have permission
+        # Create request with the test image
+        request = RequestFactory().post("/", {"upload": image_file})
 
-    @staticmethod
-    @patch("django.conf.settings.DJ_CKE_STAFF_ONLY_IMAGE_UPLOADS", new=True)
-    def test_non_staff_user_with_setting():
-        request = RequestFactory().get("/")
-        request.user = AnonymousUser()
+        result_url = handle_uploaded_image(request)
+        assert result_url == settings.MEDIA_URL + "test_image.jpg"
 
-        assert has_permission_to_upload_images(request) is False
+    @override_settings(
+        DJ_CKE_IMAGE_STORAGE="blog.storage.ImageCustomFileSystemStorage",
+        STORAGES={
+            "default": {"BACKEND": "django.core.files.storage.FileSystemStorage"},
+        },
+    )
+    def test_image_custom_file_storage_upload(self):
+        # Create test image data
+        image_data = b"Dummy image data"
+        image_file = SimpleUploadedFile("test_image.jpg", image_data)
 
-    @staticmethod
-    @patch("django.conf.settings.DJ_CKE_STAFF_ONLY_IMAGE_UPLOADS", new=True)
-    def test_staff_user():
-        request = RequestFactory().get("/")
-        request.user = MagicMock(is_staff=True)  # Simulate staff user
+        # Create request with the test image
+        request = RequestFactory().post("/", {"upload": image_file})
 
-        assert has_permission_to_upload_images(request) is True
+        result_url = handle_uploaded_image(request)
+        # assert 1 == 2
+        assert result_url == f"tests/custom/path/{image_file.name}"
 
+    def tearDown(self):
+        """
+        Overriding the TearDown function from TestCase for deleting the test file
+        """
 
-class TestImageVerify:
-    @staticmethod
-    def test_valid_filetype(valid_png_image):
+        folder = settings.MEDIA_ROOT
+        for filename in os.listdir(folder):
+            file_path = os.path.join(folder, filename)
+            try:
+                if "test" in file_path:
+                    if os.path.isfile(file_path) or os.path.islink(file_path):
+                        os.unlink(file_path)
+                    elif os.path.isdir(file_path):
 
-        image_verify(image=valid_png_image)  # Should execute without raising exceptions
+                        shutil.rmtree(file_path)
+            # try:
+            #     shutil.rmtree(settings.MEDIA_ROOT + "/tests/")
 
-    @staticmethod
-    def test_invalid_filetypes(invalid_jpg_image):
-        """invalid_jpg_image is a pdf file with jpg extension."""
-
-        with pytest.raises(InvalidImageTypeError):
-            image_verify(invalid_jpg_image)
-
-    @staticmethod
-    def test_corrupt_png(corrupted_png_image):
-        """corrupted_png_image is a png with random bytes changed."""
-        with pytest.raises(PillowImageError):
-            image_verify(corrupted_png_image)
+            except FileNotFoundError:
+                pass  # File might have not been created in the test
--- example/blog/tests/test_upload_file.py
+++ example/blog/tests/test_upload_file.py
@@ -1,7 +1,11 @@
+"""test only staff can upload file"""
 from django.test import override_settings
 from django.urls import reverse
 
 
+@override_settings(
+    DJ_CKE_STAFF_ONLY_IMAGE_UPLOADS=True,
+)
 def test_upload_file(admin_client, valid_png_image):
     with valid_png_image as upload:
         response = admin_client.post(
@@ -13,7 +17,8 @@ def test_upload_file(admin_client, valid_png_image):
 
 
 @override_settings(
-    CKEDITORS_FILE_STORAGE="storages.backends.gcloud.GoogleCloudStorage",
+    DJ_CKE_STAFF_ONLY_IMAGE_UPLOADS=True,
+    DJ_CKE_IMAGE_STORAGE="storages.backends.gcloud.GoogleCloudStorage",
     GS_BUCKET_NAME="test",
 )
 def test_upload_file_to_google_cloud(admin_client, valid_png_image, settings):
