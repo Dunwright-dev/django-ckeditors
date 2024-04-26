abc52a2e1fdf002325221e4e8b3f3adbd86ac8a1
imAsparky <mark.sevelj@dunwright.com.au>
2024-04-27 04:03:06 +0800

    refactor(settings): Update to match apps ready #41
    Some settings have been renamed when apps ready function was added to improve readability.
    
    Settings have been removed now defaults added to the apps ready function.
    
    closes #41

--- example/blog/blog/settings.py
+++ example/blog/blog/settings.py
@@ -24,12 +24,8 @@ SECRET_KEY = "mr3e&=yzha$_#-#1=ro@*rfsgm_k-ka%w$!=xgx1t@$9g!pz(_"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+ALLOWED_HOSTS = ["127.0.0.1", "127.4.5.6"]
 
-DJ_CKE_STAFF_ONLY_IMAGE_UPLOADS: bool = False
-
-ALLOWED_HOSTS = [
-    "127.0.0.1",
-]
 
 # Application definition
 
@@ -89,7 +85,7 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",  # noqa: E501
     },
     {
         "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
@@ -125,201 +121,10 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static_files"),
 ]
 
-customColorPalette = [
-    {"color": "hsl(4, 90%, 58%)", "label": "Red"},
-    {"color": "hsl(340, 82%, 52%)", "label": "Pink"},
-    {"color": "hsl(291, 64%, 42%)", "label": "Purple"},
-    {"color": "hsl(262, 52%, 47%)", "label": "Deep Purple"},
-    {"color": "hsl(231, 48%, 48%)", "label": "Indigo"},
-    {"color": "hsl(207, 90%, 54%)", "label": "Blue"},
-]
 
-CKEDITORS_CONFIGS = {
-    "default": {
-        "removePlugins": ["WordCount"],
-        "toolbar": [
-            "heading",
-            "|",
-            "bold",
-            "italic",
-            "link",
-            "bulletedList",
-            "numberedList",
-            "blockQuote",
-        ],
-    },
-    "comment": {
-        "language": {"ui": "en", "content": "ar"},
-        "toolbar": [
-            "heading",
-            "|",
-            "bold",
-            "italic",
-            "link",
-            "bulletedList",
-            "numberedList",
-            "blockQuote",
-        ],
-    },
-    "extends": {
-        "language": "ru",
-        "blockToolbar": [
-            "paragraph",
-            "heading1",
-            "heading2",
-            "heading3",
-            "|",
-            "bulletedList",
-            "numberedList",
-            "|",
-            "blockQuote",
-        ],
-        "toolbar": {
-            "items": [
-                "heading",
-                "horizontalLine",
-                "codeBlock",
-                "htmlEmbed",
-                "|",
-                "outdent",
-                "indent",
-                "|",
-                "bold",
-                "italic",
-                "link",
-                "underline",
-                "strikethrough",
-                "code",
-                "subscript",
-                "superscript",
-                "highlight",
-                "|",
-                "bulletedList",
-                "numberedList",
-                "todoList",
-                "|",
-                "blockQuote",
-                "linkImage",
-                "insertImage",
-                "|",
-                "fontSize",
-                "fontFamily",
-                "fontColor",
-                "fontBackgroundColor",
-                "mediaEmbed",
-                "removeFormat",
-                "insertTable",
-                "sourceEditing",
-                "style",
-            ],
-            "shouldNotGroupWhenFull": True,
-        },
-        "image": {
-            "toolbar": [
-                "imageTextAlternative",
-                "|",
-                "imageStyle:alignLeft",
-                "imageStyle:alignRight",
-                "imageStyle:alignCenter",
-                "imageStyle:side",
-                "|",
-            ],
-            "styles": [
-                "full",
-                "side",
-                "alignLeft",
-                "alignRight",
-                "alignCenter",
-            ],
-        },
-        "table": {
-            "contentToolbar": [
-                "tableColumn",
-                "tableRow",
-                "mergeTableCells",
-                "tableProperties",
-                "tableCellProperties",
-                "toggleTableCaption",
-            ],
-            "tableProperties": {
-                "borderColors": customColorPalette,
-                "backgroundColors": customColorPalette,
-            },
-            "tableCellProperties": {
-                "borderColors": customColorPalette,
-                "backgroundColors": customColorPalette,
-            },
-        },
-        "heading": {
-            "options": [
-                {
-                    "model": "paragraph",
-                    "title": "Paragraph",
-                    "class": "ck-heading_paragraph",
-                },
-                {
-                    "model": "heading1",
-                    "view": "h1",
-                    "title": "Heading 1",
-                    "class": "ck-heading_heading1",
-                },
-                {
-                    "model": "heading2",
-                    "view": "h2",
-                    "title": "Heading 2",
-                    "class": "ck-heading_heading2",
-                },
-                {
-                    "model": "heading3",
-                    "view": "h3",
-                    "title": "Heading 3",
-                    "class": "ck-heading_heading3",
-                },
-            ],
-        },
-        "list": {
-            "properties": {
-                "styles": True,
-                "startIndex": True,
-                "reversed": True,
-            },
-        },
-        "link": {"defaultProtocol": "https://"},
-        "htmlSupport": {
-            "allow": [
-                {"name": "/.*/", "attributes": True, "classes": True, "styles": True},
-            ],
-        },
-        "mention": {
-            "feeds": [
-                {
-                    "marker": "@",
-                    "feed": [
-                        "@Barney",
-                        "@Lily",
-                        "@Marry Ann",
-                        "@Marshall",
-                        "@Robin",
-                        "@Ted",
-                    ],
-                    "minimumCharacters": 1,
-                },
-            ],
-        },
-        "style": {
-            "definitions": [
-                {"name": "Article category", "element": "h3", "classes": ["category"]},
-                {"name": "Info box", "element": "p", "classes": ["info-box"]},
-            ],
-        },
-    },
-}
 STORAGES = {
-    "default": {"BACKEND": "articles.storage.CustomStorage"},
+    "default": {"BACKEND": "django.core.files.storage.FileSystemStorage"},
     "staticfiles": {
         "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
     },
 }
-CKEDITORS_CUSTOM_CSS = "custom.css"
-
-CSRF_COOKIE_NAME = "new_csrf_cookie_name"
