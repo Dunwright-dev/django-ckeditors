8b77cbe546f135299638436553b9adfa9cac44c2
imAsparky <mark.sevelj@dunwright.com.au>
2024-04-27 04:07:31 +0800

    refactor(helpers): Add options for urls and storage #40 #42
    Refactor get storage class to return an object so tests can be conducted using @override_settings.
    
    Add option to use a custom url for saving CKE image file.
    
    closes #40
    
    closes #42

---
+++ example/blog/blog/storage.py
@@ -0,0 +1,36 @@
+"""blog custom storage module."""
+from django.core.files.storage import FileSystemStorage
+
+
+class ImageCustomFileSystemStorage(FileSystemStorage):
+    """
+    Overrides save and url methods to add a custom path prefix to
+    file URLs for your testing environment.
+    """
+
+    def save(self, name, content, max_length=None):
+        """
+        Overrides the save method. We'll leave the saving logic to the base
+        FileSystemStorage class.
+        """
+        new_name = f"tests/custom/path/{name}"
+        return super().save(
+            name=new_name,
+            content=content,
+            max_length=max_length,
+        )
+
+    def url(self, name):
+        """Overrides the URL generation for saved files."""
+        return name
+
+
+'''
+# def image_url_handler(request):
+#     """Return a unique image path_to/filename"""
+#     img = request.FILES["upload"]
+#     fpath = pathlib.Path(img.name)
+#     new_name = "new_img_name"
+#
+#     return f"images/{new_name}{fpath.suffix}"
+'''
